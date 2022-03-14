467d6ddf4c5ad01dd865e1077921167a5228d659
ucfund <ucfund@protonmail.com>
2022-03-14 11:32:51 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+$(function() {
+  // 导航跟随
+  $('#nav').onePageNav({
+    changeHash: true,
+    filter: ':not(.external)'
+  })
+  setTimeout(function() {
+    document.documentElement.scrollTop += 1
+    document.body.scrollTop += 1
+  }, 1)
+  // 移动端点击展开收缩菜单
+  $('#collapse1').on('click', function() {
+    $('.my-navbar-collapse').stop().slideToggle()
+  })
+  // PC端鼠标悬浮菜单
+  $('.options-btn-wrap').hover(function() {
+    $(this).children('.menus-wrap').stop().fadeIn()
+  }, function() {
+    $(this).children('.menus-wrap').stop().fadeOut()
+  })
+
+  // 导航自适应
+  $(window).on('resize', setNavContainer)
+  setNavContainer()
+  function setNavContainer() {
+    if (window.innerWidth >= 992) {
+      $('.navbar > div').addClass('container').removeClass('container-fluid')
+    } else {
+      $('.navbar > div').addClass('container-fluid').removeClass('container')
+    }
+  }
+})
+
+;(function() {
+  // 页面动画效果
+  AOS.init()
+})()
+
+$(function() {
+  // 轮播
+  var swiperNavs = $('.swiper-nav a')
+
+  swiperNavs.on('click', function() {
+    var index = $(this).parent().index()
+    roadmapSwiper.slideToLoop(index)
+  })
+
+  var roadmapSwiper = new Swiper ('.roadmap-swiper .swiper-container', {
+    loop: true, // 循环模式选项
+    autoHeight: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on:{
+      slideChangeTransitionStart: function(){
+        swiperNavs.eq(this.realIndex)
+          .addClass('active')
+          .parent()
+          .siblings()
+          .children()
+          .removeClass('active')
+      }
+    }
+  })
+})
